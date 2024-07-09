9b55435212a03a36c1f2582e5f755ecc17c39a8e
Arvind Singh <arvindsinghnitp@gmail.com>
2024-07-09 10:40:20 +0530

    Create mapLimit.js

---
+++ mapLimit.js
@@ -0,0 +1,28 @@
+async function mapLimit(arr, limit, fn) {
+  let arr_index = 0;
+  const result = [];
+
+  while (arr_index < arr.length) {
+    let limit_ind = 0;
+    while (limit_ind < limit && arr_index < arr.length) {
+      result[arr_index] = await processItem(arr, arr_index, fn);
+      arr_index++;
+      limit_ind++;
+    }
+  }
+
+  return result;
+}
+
+async function processItem(arr, arr_index, fn, result) {
+  console.log("added item ", arr_index);
+  const data = await new Promise((res) => res(fn(arr[arr_index])));
+  return data;
+}
+
+async function main() {
+  const result = await mapLimit([1, 2, 3, 4, 5], 5, async (item) => 2 * item);
+  console.log(result);
+}
+
+main();
